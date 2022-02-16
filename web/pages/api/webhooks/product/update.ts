f3258f817e46f2c8ae2573c7e9328f1b7c13a0b4
jakak <74316018+jaka-k@users.noreply.github.com>
2022-02-16 21:05:41 +0100

    Added console log to test Sanity webhook

--- web/pages/api/webhooks/product/update.ts
+++ web/pages/api/webhooks/product/update.ts
@@ -3,8 +3,10 @@ import axios from 'axios';
 export const updateProducts = async () => {
   try {
     const { data } = await axios.get('');
-    return { status: 'Successful', apartments: data };
+    console.log('This is the API console log: if Successful')
+    return { status: 'Successful', product: data };
   } catch (err) {
+    console.log('This is the API console log: if Fail')
     return { status: 'Error', err: err?.response?.data };
   }
 };
