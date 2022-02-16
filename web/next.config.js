0b30acf2b614f4b698f5349b18fd79ddaf4d2688
Domen <domen.lisjak@inova.si>
2022-02-16 19:31:36 +0100

    add featured products to home

--- web/next.config.js
+++ web/next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  async redirects() {
+    return [
+      {
+        source: '/category',
+        destination: '/categories',
+        permanent: true,
+      },
+    ]
+  },
 }
