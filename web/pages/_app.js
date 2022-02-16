872bc6c0ec1bd67d8e087df522c5485cdde7e55f
Domen <domen.lisjak@inova.si>
2022-02-16 19:36:08 +0100

    add navbar

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,11 +1,14 @@
-
+import Navbar from '../components/Navbar';
 import '../styles/main.css';
 
 const MyApp = ({ Component, pageProps, router }) => {
   const { route } = router;
 
   return (
-    <Component {...pageProps} key={route} />
+    <>
+      <Navbar />
+      <Component {...pageProps} key={route} />
+    </>
   );
 };
 
