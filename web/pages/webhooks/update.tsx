9479dfa53cf419933058ffe8b8dd06204b599355
Domen <domen.lisjak@inova.si>
2022-02-16 16:36:32 +0100

    add monorepo

---
+++ web/pages/webhooks/update.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+const Update = () => {
+  useEffect(() => {
+    axios.get('');
+  }, []);
+
+  return <div>Update</div>;
+};
+
+export default Update;
