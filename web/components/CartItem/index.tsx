0499518a5b4db12804e9e290df30c148b324e59a
Domen <domen.lisjak@inova.si>
2022-02-17 21:17:06 +0100

    wait for data for products

--- web/components/CartItem/index.tsx
+++ web/components/CartItem/index.tsx
@@ -3,6 +3,7 @@ import { useSanityProduct } from '../../queries/sanity/product';
 const CartItem = ({ item }) => {
   const { product, isLoading, isError } = useSanityProduct(item.id);
 
+  if (isLoading) return <div>isLoading...</div>;
   return (
     <div className={`flex p-4 border-2 ${isError ? 'border-rose-600' : 'border-slate-200'}`}>
       <div className="mr-4">{product.title}</div>
