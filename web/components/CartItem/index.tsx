7f7c6b190f6368ef52dc5333ac98075653ed89a4
Domen <domen.lisjak@inova.si>
2022-02-17 21:13:04 +0100

    remove unneeded file

--- web/components/CartItem/index.tsx
+++ web/components/CartItem/index.tsx
@@ -2,12 +2,11 @@ import { useSanityProduct } from '../../queries/sanity/product';
 
 const CartItem = ({ item }) => {
   const { product, isLoading, isError } = useSanityProduct(item.id);
-  const { title, defaultProductVariant } = product;
 
   return (
     <div className={`flex p-4 border-2 ${isError ? 'border-rose-600' : 'border-slate-200'}`}>
-      <div className="mr-4">{title}</div>
-      <div className="mr-4">{defaultProductVariant.price}</div>
+      <div className="mr-4">{product.title}</div>
+      <div className="mr-4">{product.defaultProductVariant.price}</div>
       <div className="mr-4">{item.quantity}</div>
     </div>
   );
