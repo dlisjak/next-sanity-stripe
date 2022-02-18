614349a2ef5b8d32b8e7ac161daccc5e63128aca
jakak <74316018+jaka-k@users.noreply.github.com>
2022-02-18 11:34:25 +0100

    Ngrok middleware for Sanity webhook

--- web/pages/api/webhooks/product/update.ts
+++ web/pages/api/webhooks/product/update.ts
@@ -3,16 +3,16 @@ import axios from 'axios';
 export const updateProducts = async () => {
   try {
     const { data } = await axios.get('');
-    console.log('This is the API console log: if Successful')
     return { status: 'Successful', product: data };
   } catch (err) {
-    console.log('This is the API console log: if Fail')
     return { status: 'Error', err: err?.response?.data };
   }
 };
 
 export default async function handler(req, res) {
-  const response = await updateProducts();
+  axios.post('https://c981-86-58-45-210.eu.ngrok.io', req)
+  // const response = await updateProducts();
 
-  return res.status(200).json(response);
+
+  return res.status(200).json(res);
 }
